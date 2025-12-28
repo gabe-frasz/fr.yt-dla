8295234129eb78466c3a71419c53a5472f4e89f5
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-12-27 21:39:03 -0300

    refactor: download on the background

--- scripts/termux-url-opener.sh
+++ scripts/termux-url-opener.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 url=$1
-audio_output_dir="~/storage/shared/Music"
-video_output_dir="~/storage/shared/Movies"
+audio_output_dir="$HOME/storage/shared/Music"
+video_output_dir="$HOME/storage/shared/Movies"
+log_file="$HOME/yt-dlp.log"
 
 if [ -z "$url" ]; then
-    termux-toast "No URL provided!"
-    exit 1
+  termux-toast "No URL provided!"
+  exit 1
+fi
+
+if [ ! -f "$HOME/yt-dlp.log" ]; then
+  touch "$HOME/yt-dlp.log"
 fi
 
 download_options=(
@@ -17,47 +22,60 @@ download_options=(
   "Video - Fast (360p)"
 )
 
-json_options_response=(termux-dialog radio -t "Download as" -v "download_options[@]")
-option_index=$(echo "$json_options_response" | jq ".index")
+old_ifs=$IFS
+IFS=,
+options_str="${download_options[*]}"
+IFS=$old_ifs
+
+json_options_response=$(termux-dialog radio -t "Download as" -v "$options_str")
+option_index=$(echo "$json_options_response" | jq -r ".index")
 option=$(echo "$json_options_response" | jq -r ".text")
 
-if [ "$option_index" == "-1" ]; then
-    termux-toast "Download cancelled"
-    exit 0
+if [[ "$option_index" == "null" ]] || [[ $option_index -lt 0 ]] || [[ -z "$option" ]]; then
+  termux-toast "Download cancelled"
+  exit 0
 fi
 
 case $option_index in
-    0) ARGS="-f ba[ext=m4a] --extract-audio" ;; # Audio M4A
-    1) ARGS="-x --audio-format mp3 --audio-quality 0" ;; # Audio MP3
-    2) ARGS="-f bv*+ba/b" ;; # Max
-    3) ARGS="-f bestvideo[height<=720]+bestaudio/best[height<=720]" ;; # 720p
-    4) ARGS="-f bestvideo[height<=360]+bestaudio/best[height<=360]" ;; # 480p
+  0) yt_dlp_args="-f ba[ext=m4a] --extract-audio" ;; # Audio M4A
+  1) yt_dlp_args="-x --audio-format mp3 --audio-quality 0" ;; # Audio MP3
+  2) yt_dlp_args="-f bv*+ba/b" ;; # Video Max
+  3) yt_dlp_args="-f bestvideo[height<=720]+bestaudio/best[height<=720]" ;; # Video 720p
+  4) yt_dlp_args="-f bestvideo[height<=360]+bestaudio/best[height<=360]" ;; # Video 360p
 esac
 
-mkdir -p "$audio_output_dir"
-mkdir -p "$video_output_dir"
-
-if [ $option_index < 3 ]; then
-    cd "$audio_output_dir"
+if [[ $option_index -lt 2 ]]; then
+  cd $audio_output_dir
 else
-    cd "$video_output_dir"
+  cd $video_output_dir
 fi
 
-termux-notification --id 1 --title "yt-dlp" --content "Downloading: $url"
+(
+  termux-wake-lock
 
-# yt-dlp
-# --no-mtime: uses today date instead of video upload date
-# --restrict-filenames: remove special chars and spaces
-# -o: output template
+  termux-notification --id 1 --title "yt-dla" --content "Downloading: $url"
 
-yt-dlp $ARGS \
---no-mtime \
---restrict-filenames \
--o "%(title)s [%(id)s].%(ext)s" \
-"$URL"
+  echo "####### Downloading: $url #######" >> $log_file
 
-if [ $? -eq 0 ]; then
-    termux-notification --id 1 --title "yt-dlp" --content "Successfully downloaded: $url"
-else
-    termux-notification --id 1 --title "yt-dlp" --content "Error trying to download: $url" --priority high
-fi
+  # yt-dlp
+  # --no-mtime: uses today date instead of video upload date
+  # --restrict-filenames: remove special chars and spaces
+  # -o: output template
+
+  yt-dlp $yt_dlp_args \
+  --no-mtime \
+  --restrict-filenames \
+  --exec "termux-media-scan {}" \
+  -o "%(title)s.%(ext)s" \
+  "$url" >> $log_file 2>&1
+
+  if [ $? -eq 0 ]; then
+    termux-notification --id 1 --title "yt-dla" --content "Successfully downloaded: $url"
+  else
+    termux-notification --id 1 --title "yt-dla" --content "Error trying to download: $url" --priority high
+  fi
+
+  termux-wake-unlock
+) & disown
+
+exit 0
